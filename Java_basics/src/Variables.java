0aca7d020fc735dce79c38593379162c42c59978
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-20 18:34:48 -0500

    started Udemy java certification course, intro with variable declaration and Eclipse IDE walkthroughs. Learning the differences between VS and Eclipse."

---
+++ Java_basics/src/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(x);
+        System.out.println(x + 4);
+        String words = "test string";
+        System.out.println(words);
+    }
+}
