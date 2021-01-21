493764b119ff67f88e0d28008cec0b7ea2e597f3
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-20 19:46:30 -0500

    look up stringbuilder usage before continuing to next section

---
+++ Java_basics/src/LearningArrays.java
@@ -0,0 +1,31 @@
+public class LearningArrays {
+    public static void main(String[] args) {
+        int[] values = new int[100];
+        values[0] = 1000;
+        System.out.println(values[0]);
+
+        double[] decimalValues = new double[100];
+        decimalValues[0] = 199.75;
+        System.out.println("Populated value: " + decimalValues[0]);
+        System.out.println("Unpopulated/Default value: " + decimalValues[1]);
+
+        String[] words = new String[] { "Hello", "World" };
+        System.out.println(words[0] + " " + words[1]);
+
+        String[] wordsWithDefinedLength = new String[2];
+        wordsWithDefinedLength[0] = "Hello";
+        wordsWithDefinedLength[1] = "World";
+
+        // testing for loop for concatenation:
+        String sentence = ""; // Had to initialize this with a value to run.
+        for (int i = 0; i < wordsWithDefinedLength.length; i++) {
+            sentence += wordsWithDefinedLength[i] + " ";
+        }
+        System.out.println(sentence);
+
+        // StringBuilder warning?
+        // Not understanding StringBuilder, link for reference:
+        // https://docs.oracle.com/javase/tutorial/java/data/buffers.html
+
+    }
+}
---
+++ Java_basics/src/VariablesContinued.java
@@ -0,0 +1,11 @@
+public class VariablesContinued {
+    public static void main(String[] args) {
+        int var = 1000000000; // Maximum length for int data type.
+        long bigNumber = 10000000000000000L;
+        short smallNumber = 32767; // Maximum value of short is 32767. Can be negative.
+        byte smallestNumber = 127; // Maximum value of byte is 127. Can be negative.
+        double decimalVariable = 394.003; // For decimal values.
+        boolean condition = true; // Boolean value.
+        char singleCharacters = 'l'; // This required single quotes, failed with double.
+    }
+}
