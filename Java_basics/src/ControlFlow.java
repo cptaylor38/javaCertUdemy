b59c841a3d5d321a9c47b540e84250f21ba4989f
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-22 11:57:47 -0500

    pausing for interview, then work, then deeper dive into methods!

--- Java_basics/src/ControlFlow.java
+++ Java_basics/src/ControlFlow.java
@@ -36,5 +36,32 @@ public class ControlFlow {
         }
 
         System.out.println(opinion);
+
+        int month = 6;
+        String monthString;
+
+        switch (month) {
+            case 1:
+                monthString = "January";
+                break;
+            case 2:
+                monthString = "February";
+                break;
+            case 3:
+                monthString = "March";
+                break;
+            case 4:
+                monthString = "April";
+                break;
+            case 5:
+                monthString = "May";
+                break;
+            case 6:
+                monthString = "June";
+                break;
+            default:
+                monthString = "Unknown";
+                break;
+        }
     }
 }
---
+++ Java_basics/src/LearningMethods.java
@@ -0,0 +1,9 @@
+public class LearningMethods {
+    public static void main(String[] args) {
+        printText("Some data");
+    }
+
+    public static void printText(String arg) {
+        System.out.println(arg);
+    }
+}
